91b301bf9f8aac09d0e0e3df370b79263403ecc0
Hiroyuki Sogo <hsogo12600@gmail.com>
2021-01-27 16:03:01 +0900

    NEW: support for configuration file

--- PortablePsychopyLauncher/PortablePsychopyLauncher.cpp
+++ PortablePsychopyLauncher/PortablePsychopyLauncher.cpp
@@ -5,8 +5,96 @@
 //
 #include <windows.h>
 #include <tchar.h>
+#include <shlwapi.h>
+#include <fstream>
 
 #define TCHAR_SIZE(buf) (sizeof(buf)/sizeof(TCHAR))
+
+
+//From: http://www.hiramine.com/programming/windows/copydirectory.html
+BOOL CopyDirectory(LPCTSTR lpExistingDirectoryName,
+	LPCTSTR lpNewDirectoryName)
+{
+	// check input values
+	if (NULL == lpExistingDirectoryName
+		|| NULL == lpNewDirectoryName)
+	{
+		return FALSE;
+	}
+
+	// preserve directory name (adding '\' if it doesn't exist)
+	TCHAR szDirectoryPathName_existing[_MAX_PATH];
+	_tcsncpy_s(szDirectoryPathName_existing, _MAX_PATH, lpExistingDirectoryName, _TRUNCATE);
+	if ('\\' != szDirectoryPathName_existing[_tcslen(szDirectoryPathName_existing) - 1])
+	{	// Add '\'
+		_tcsncat_s(szDirectoryPathName_existing, _MAX_PATH, _T("\\"), _TRUNCATE);
+	}
+	TCHAR szDirectoryPathName_new[_MAX_PATH];
+	_tcsncpy_s(szDirectoryPathName_new, _MAX_PATH, lpNewDirectoryName, _TRUNCATE);
+	if ('\\' != szDirectoryPathName_new[_tcslen(szDirectoryPathName_new) - 1])
+	{	// Add '\'
+		_tcsncat_s(szDirectoryPathName_new, _MAX_PATH, _T("\\"), _TRUNCATE);
+	}
+
+	if (-1 == _taccess(szDirectoryPathName_existing, 0))
+	{
+		return FALSE;
+	}
+	if (0 == _tcsicmp(szDirectoryPathName_existing, szDirectoryPathName_new))
+	{
+		return FALSE;
+	}
+
+	// Create new directory
+	CreateDirectory(szDirectoryPathName_new, NULL);
+
+	// Parse files in the directory
+	TCHAR szFindFilePathName[_MAX_PATH];
+	_tcsncpy_s(szFindFilePathName, _MAX_PATH, szDirectoryPathName_existing, _TRUNCATE);
+	_tcsncat_s(szFindFilePathName, _T("*"), _TRUNCATE);
+
+	WIN32_FIND_DATA		fd;
+	HANDLE hFind = FindFirstFile(szFindFilePathName, &fd);
+	if (INVALID_HANDLE_VALUE == hFind)
+	{	// FindFirstFile failed
+		return FALSE;
+	}
+
+	do
+	{
+		if ('.' != fd.cFileName[0])
+		{
+			TCHAR szFoundFilePathName_existing[_MAX_PATH];
+			_tcsncpy_s(szFoundFilePathName_existing, _MAX_PATH, szDirectoryPathName_existing, _TRUNCATE);
+			_tcsncat_s(szFoundFilePathName_existing, _MAX_PATH, fd.cFileName, _TRUNCATE);
+
+			TCHAR szFoundFilePathName_new[_MAX_PATH];
+			_tcsncpy_s(szFoundFilePathName_new, _MAX_PATH, szDirectoryPathName_new, _TRUNCATE);
+			_tcsncat_s(szFoundFilePathName_new, _MAX_PATH, fd.cFileName, _TRUNCATE);
+
+			if (FILE_ATTRIBUTE_DIRECTORY & fd.dwFileAttributes)
+			{	// Call CopyDirctory recursively if the target is directory
+				if (!CopyDirectory(szFoundFilePathName_existing, szFoundFilePathName_new))
+				{
+					FindClose(hFind);
+					return FALSE;
+				}
+			}
+			else
+			{	// Copy file
+				if (!CopyFile(szFoundFilePathName_existing, szFoundFilePathName_new, FALSE))
+				{
+					FindClose(hFind);
+					return FALSE;
+				}
+			}
+		}
+	} while (FindNextFile(hFind, &fd));
+
+	FindClose(hFind);
+
+	return TRUE;
+}
 
 
 BOOL findDirectory(LPCTSTR lpTargetDir, LPCTSTR lpSearchName, LPTSTR foundDirName, int n)
@@ -43,8 +131,46 @@ int APIENTRY _tWinMain(
 	TCHAR szPythonDirFull[1024];
 	TCHAR szPsychoPyDir[512];
 	TCHAR szPath[2048];
+
+	TCHAR szPsychoPyConfigDir[1024];
+	TCHAR szAppDataDir[512];
+	size_t requiredSize;
+
 	STARTUPINFO si;
 	PROCESS_INFORMATION pi;
+
+	BOOL show_normal_termination_dialog = true;
+	BOOL copy_config = false;
+
+	// read config file
+	std::fstream fs;
+	fs.open("pplaunch_config.txt", std::ios::in);
+	if (fs.is_open())
+	{
+		char buff[256], *p, *pp;
+		int param;
+		while (fs.getline(buff, sizeof(buff)))
+		{
+			//If ":" is not included, this line is not option.
+			if ((p = strchr(buff, ':')) == NULL) continue;
+
+			//remove space/tab
+			*p = '\0';
+			while (*(p - 1) == 0x09 || *(p - 1) == 0x20)
+			{
+				p--;
+				*p = '\0';
+			}
+			while (*(p + 1) == 0x09 || *(p + 1) == 0x20) p++;
+
+			// get first character of the parameter value
+			p += 1;
+			param = strtol(p, &pp, 10);
+
+			if (strcmp(buff, "show_normal_termination_dialog") == 0) show_normal_termination_dialog = (BOOL)param;
+			else if (strcmp(buff, "copy_config") == 0) copy_config = (BOOL)param;
+		}
+	}
 
 	GetStartupInfo(&si);
 	//si.dwFlags = STARTF_USESHOWWINDOW;
@@ -122,8 +248,31 @@ int APIENTRY _tWinMain(
 	WINPYWORKDIR = C:\Users\Hiroyuki\Desktop\WinPython-64bit-3.6.7.0-PsychoPy3.0.0\scripts\..\Notebooks
 	*/
 
-
 	_stprintf_s(szCmd, _T("\"%s\\%s\\pythonw.exe\" -m psychopy.app.psychopyApp"), szWinPythonDir, szPythonDir);
+
+	if (copy_config) {
+		// Get full path of %APPDATA%\psychopy3 directory	
+		_tgetenv_s(&requiredSize, NULL, 0, _T("APPDATA"));
+		if (requiredSize == 0) {
+			return -1;
+		}
+		else if (requiredSize >= TCHAR_SIZE(szAppDataDir)) {
+			return -1;
+		}
+		_tgetenv_s(&requiredSize, szAppDataDir, requiredSize, _T("APPDATA"));
+
+		// Copy configuration to %APPDATA%\psychopy3
+		_stprintf_s(szPsychoPyConfigDir, _T("%s\\psychopy3\\"), szAppDataDir);
+		if (PathIsDirectory(_T(".\\psychopy3\\"))) {
+			CopyDirectory(_T(".\\psychopy3\\"), szPsychoPyConfigDir);
+		}
+
+		// Copy configuration to %APPDATA%\psychopy3
+		_stprintf_s(szPsychoPyConfigDir, _T("%s\\psychopy3\\"), szAppDataDir);
+		if (PathIsDirectory(_T(".\\psychopy3\\"))) {
+			CopyDirectory(_T(".\\psychopy3\\"), szPsychoPyConfigDir);
+		}
+	}
 
 	// Start PsychoPy
 	if (!CreateProcess(
@@ -133,8 +282,9 @@ int APIENTRY _tWinMain(
 		MessageBox(NULL, szTmp, _T("Error"), MB_ICONERROR);
 		return -1;
 	}
-
+	// Waiting...
 	DWORD r = WaitForSingleObject(pi.hProcess, INFINITE);
+
 	switch (r) {
 	case WAIT_FAILED:
 		MessageBox(NULL, _T("Wait failed (WAIT_FAILED)"), _T("Error"), MB_ICONERROR);
@@ -165,7 +315,25 @@ int APIENTRY _tWinMain(
 		return -1;
 	}
 
-	MessageBox(NULL, _T("PortablePsychoPy is successfully closed."), _T("Information"), MB_ICONINFORMATION);
+	//Normal termination
+	if (copy_config) {
+		//Copy configuration from %APPDATA%\psychopy3
+		_stprintf_s(szPsychoPyConfigDir, _T("%s\\psychopy3\\"), szAppDataDir);
+		if (PathIsDirectory(szPsychoPyConfigDir)) {
+			CopyDirectory(szPsychoPyConfigDir, _T(".\\psychopy3\\"));
+		}
+
+		//Copy configuration from %APPDATA%\psychopy3
+		_stprintf_s(szPsychoPyConfigDir, _T("%s\\psychopy3\\"), szAppDataDir);
+		if (PathIsDirectory(szPsychoPyConfigDir)) {
+			CopyDirectory(szPsychoPyConfigDir, _T(".\\psychopy3\\"));
+		}
+	}
+
+
+	if (show_normal_termination_dialog) {
+		MessageBox(NULL, _T("PortablePsychoPy is successfully closed."), _T("Information"), MB_ICONINFORMATION);
+	}
 	return exitCode;
 
 }
